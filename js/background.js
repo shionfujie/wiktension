de454b87cda1e117db1fafcd49cdee12be11f9c5
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-11 21:18:50 +0900

    accept separate request for select reply

--- js/background.js
+++ js/background.js
@@ -1,42 +1,49 @@
-chrome.runtime.onMessageExternal.addListener((request, sender, response) => {
-  if (request.type === "action spec") {
-    response({
-      name: actionSpec.name,
-      actions: Object.entries(actionSpec.actions).map(
-        ([name, { displayName }]) => {
-          return { name, displayName };
-        }
-      )
-    });
-  } else if (request.type === "execute action") {
-    switch (request.action.name) {
-      case "switch language":
-        getLanguages(languages => {
-          chrome.runtime.sendMessage(
-            sender.id,
-            {
-              type: "select",
-              options: Object.keys(languages).map(key => ({value: key, displayName: key}))
-            },
-            response => {
-              if (response.cancelled || response.selected === undefined) return;
-              console.log(response.selected)
-                requestNavigateTo(languages[response.selected])
+chrome.runtime.onMessageExternal.addListener(
+  (request, sender, sendResponse) => {
+    console.log(request);
+    switch (request.type) {
+      case "action spec":
+        sendResponse({
+          name: actionSpec.name,
+          actions: Object.entries(actionSpec.actions).map(
+            ([name, { displayName }]) => {
+              return { name, displayName };
             }
-          );
+          )
         });
         break;
+      case "execute action":
+        switch (request.action.name) {
+          case "switch language":
+            getLanguages(languages => {
+              chrome.runtime.sendMessage(sender.id, {
+                type: "select",
+                options: Object.entries(languages).map(([name, url]) => ({
+                  value: url,
+                  displayName: name
+                }))
+              });
+            });
+            break;
+          default:
+            const action = actionSpec.actions[request.action.name];
+            if (action !== undefined) action.f();
+            break;
+        }
+      case "select/response":
+        if (request.cancelled || request.selected === undefined) return;
+        console.log(request.selected);
+        requestNavigateTo(request.selected);
+        break;
     }
-    const action = actionSpec.actions[request.action.name];
-    if (action !== undefined) action.f();
   }
-});
+);
 
 const actionSpec = {
   name: "Wikipedia",
   actions: {
     "switch language": {
-      f: requestSwitchLanguage
+      f: getLanguages
     }
   }
 };
@@ -46,5 +53,5 @@ function getLanguages(sendLanguages) {
 }
 
 function requestNavigateTo(url) {
-    sendMessageToActiveTab({type: "navigate", url})
+  sendMessageToActiveTab({ type: "navigate", url });
 }
