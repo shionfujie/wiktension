9129d8fff4dba97d9cf945e10317b38f079bd45d
shion.t.fujie <tonaluna32966@gmail.com>
2021-01-10 11:27:09 +0900

    take first sentence of element's text content

--- js/background.js
+++ js/background.js
@@ -44,6 +44,10 @@ const actionSpec = {
   actions: {
     "switch language": {
       f: getLanguages
+    },
+    "fragment": {
+      display: "Wikipedia: Copy Fragment",
+      f: injectFragmentExtractor
     }
   }
 };
@@ -54,4 +58,8 @@ function getLanguages(sendLanguages) {
 
 function requestNavigateTo(url) {
   sendMessageToActiveTab({ type: "navigate", url });
+}
+
+function injectFragmentExtractor() {
+  chrome.tabs.executeScript({ file: "/js/fragment.js"})
 }
---
+++ js/fragment.js
@@ -0,0 +1,56 @@
+setup();
+
+function setup() {
+    console.debug("Setting up")
+    addListeners();
+    injectCSS();
+}
+
+var capturing = undefined
+function addListeners() {
+    for (const el of document.querySelectorAll("p,div")) {
+        el.addEventListener("mouseover", decorate)
+        el.addEventListener("click", takeFirstSentence)
+    }
+}
+
+function decorate(ev) {
+    const el = ev.currentTarget
+    if (capturing !== undefined) {
+        capturing.style["background"] = ""
+    }
+    el.style["background"] = "#b3e5fc"
+    capturing = el
+    ev.stopPropagation()
+}
+
+function takeFirstSentence(ev) {
+    const sentence = sentences(ev.currentTarget.textContent)[0]
+    console.debug(sentence)
+    ev.stopPropagation()
+}
+
+function sentences(text) {
+    return text.replace(/([.!?]\s+(?=.))/g, "$1|").split(/[|]/g)
+}
+
+function teardown() {
+    if (capturing !== undefined) {
+        capturing.style["background"] = ""
+    }
+    removeListeners()
+}
+
+
+function removeListeners() {
+    for (const el of document.querySelectorAll("p,div")) {
+        el.removeEventListener("mouseover", decorate);
+        el.removeEventListener("click", takeFirstSentence)
+    }
+}
+
+// function extractFragment(e) {
+//     const el = e.currentTarget
+
+
+// }
